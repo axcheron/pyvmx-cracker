5cb8e17ab569f7e93623218925a1900674b5a80f
axcheron <anon@ymous>
2018-10-03 18:30:08 -0400

    init

---
+++ README.md
@@ -0,0 +1,97 @@
+# pyvboxdie-cracker
+
+Based on the [VMwareVMX](https://github.com/RF3/VMwareVMX) module, this tool aims to crack VMX encryption passwords. This tool crack the password by successfully decrypting the *dict* structure. If you want to fully decrypt the VMX structure, check the [VMwareVMX](https://github.com/RF3/VMwareVMX) module.
+
+## Description
+
+The VMX files (.vmx) contains the virtual machine informations, VMware VMX configuration files are encrypted when the virtual machine is
+encrypted. Here is a sample from an encrypted VMX file :
+
+```bash
+.encoding = "UTF-8"
+displayName = "Encrypted"
+encryption.keySafe = "vmware:key/list/(pair/(phrase/MA3fCocdhNc%3d/pass2key%3dPBKDF2%2dHMAC%2dSHA%2d1%3a
+cipher%3dAES%2d256%3arounds%3d1000%3asalt%3d9kxr%2bxeqo4xPz9ttPZUVFA%253d%253d,
+HMAC%2dSHA%2d1,X4sG4nJc0yeWAaSkBllAPI4nCrbO2RUE8dXHa82I4KmfNO7JjruuCrWgRRT6EUQHGQP%2bTDjPFSLHZ
+s%2bwRpFZXpjyWvJkzwFhx7UJGQriz3SCXWlwrz1zNPYAmqSXiusyFiY4js0CdabNfdFQKtLy79jDuP0%3d))"
+encryption.data = "..."
+```
+
+The KeyStore is a simple structure, and it contains the information needed by the machine to verify the password each time the user wants to start a machine or change its password. Here are some information about the *KeySafe* structure :
+
+| Name | Description |
+| ---- | ----------- | 
+| id | Identifier must be 8 bytes long and is just a random number | 
+| password_hash | Only PBKDF2-HMAC-SHA-1 algorithm for the password is supported | 
+| password_cipher | Only AES-256 encryption algorithm for the dictionary is supported | 
+| hash_round | Hash rounds | 
+| salt | The salt parameter is used with the password for the PBKDF2-HMAC-SHA-1 | 
+| config_hash | Only HMAC-SHA-1 hash algorithm for the configuration is supported | 
+| dict | Dictionary (starts with 'type=key:cipher=AES-256:key=' when successfully decrypted) | 
+
+
+## Requirements
+
+This tool requires the [pyCrypto](https://www.dlitz.net/software/pycrypto/) module. You can install it from the requirements file :
+
+```bash
+$ pip3 install -r requirements.txt
+```
+
+You also can install it directly :
+
+```bash
+$ pip3 pip install pyCrypto
+```
+
+## Install
+
+Checkout the source: `git clone https://github.com/axcheron/pyvmx-cracker.git`
+
+## Getting Started
+
+```bash
+$ python3 pyvmx-cracker.py
+usage: pyvmx-cracker.py [-h] [-v VMX] [-d DICT]
+
+Simple tool to crack VMware VMX encryption passwords
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v VMX, --vmx VMX     .vmx file
+  -d DICT, --dict DICT  password list
+
+$ python3 pyvmx-cracker.py -v sample.vmx -d wordlist.txt
+Starting pyvmx-cracker...
+
+[*] KeySafe information...
+        ID = 300ddf0a871d84d7
+        Hash = PBKDF2-HMAC-SHA-1
+        Algorithm = AES-256
+        Config Hash = HMAC-SHA-1
+        Salt = f64c6bfb17aaa38c4fcfdb6d3d951514
+
+[*] Starting bruteforce...
+        9 password tested...
+        20 password tested...
+        40 password tested...
+        111 password tested...
+        128 password tested...
+        136 password tested...
+        140 password tested...
+        154 password tested...
+        180 password tested...
+        209 password tested...
+
+[*] Password Found = Password123
+```
+
+## Resources
+
+Here are some interesting resources about this project :
+
+- https://github.com/RF3/VMwareVMX
+
+## License
+
+This project is released under the MIT License. See LICENCE file.
---
+++ pyvmx-cracker.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python3
+
+""" pyvmx-cracker.py: Simple tool to crack VMX encryption passwords """
+
+__author__ = 'axcheron'
+__license__ = 'MIT License'
+__version__ = '0.1'
+
+from urllib.parse import unquote
+from Crypto.Cipher import AES
+from binascii import hexlify
+import argparse
+import hashlib
+import random
+import base64
+import sys
+import re
+
+ks_re = '.+phrase/(.*?)/pass2key=(.*?):cipher=(.*?):rounds=(.*?):salt=(.*?),(.*?),(.*?)\)'
+
+ks_struct = {
+    'id': None,
+    'password_hash': None,
+    'password_cipher': None,
+    'hash_round': None,
+    'salt': None,
+    'config_hash': None,
+    'dict': None
+}
+
+def print_ksdata(keysafe):
+    print("[*] KeySafe information...")
+    print("\tID = %s" % keysafe['id'])
+    print("\tHash = %s" % keysafe['password_hash'])
+    print("\tAlgorithm = %s" % keysafe['password_cipher'])
+    print("\tConfig Hash = %s" % keysafe['config_hash'])
+    print("\tSalt = %s" % hexlify(keysafe['salt']).decode())
+
+
+
+def crack_keysafe(keysafe, dict):
+    wordlist = open(dict, 'r')
+    count = 0
+
+    print("\n[*] Starting bruteforce...")
+
+    for line in wordlist.readlines():
+
+        dict_key = hashlib.pbkdf2_hmac('sha1', line.rstrip().encode(), keysafe['salt'],
+                                       keysafe['hash_round'], 32)
+
+        dict_aes_iv = keysafe['dict'][:AES.block_size]
+        cipher = AES.new(dict_key, AES.MODE_CBC, dict_aes_iv)
+        dict_dec = cipher.decrypt(keysafe['dict'][AES.block_size:-20])
+
+        if random.randint(1, 20) == 12:
+            print("\t%d password tested..." % count)
+        count += 1
+
+        try:
+            if 'type=key:cipher=AES-256:key=' in dict_dec.decode():
+                print("\n[*] Password Found = %s" % line.rstrip())
+                exit(0)
+        except UnicodeDecodeError:
+            pass
+
+    print("\n[-] Password Not Found. You should try another dictionary.")
+
+
+def parse_keysafe(file):
+    try:
+        with open(file, 'r') as data:
+            lines = data.readlines()
+    except (OSError, IOError):
+        sys.exit('[-] Cannot read from file ' + data)
+
+    for line in lines:
+        if 'encryption.keySafe' in line:
+            keysafe = line
+
+    keysafe = unquote(keysafe)
+
+    match = re.match(ks_re, keysafe)
+    if not match:
+        msg = 'Unsupported format of the encryption.keySafe line:\n' + keysafe
+        raise ValueError(msg)
+
+    vmx_ks = ks_struct
+
+    vmx_ks['id'] = hexlify(base64.b64decode(match.group(1))).decode()
+    vmx_ks['password_hash'] = match.group(2)
+    vmx_ks['password_cipher'] = match.group(3)
+    vmx_ks['hash_round'] = int(match.group(4))
+    vmx_ks['salt'] = base64.b64decode(unquote(match.group(5)))
+    vmx_ks['config_hash'] = match.group(6)
+    vmx_ks['dict'] = base64.b64decode(match.group(7))
+
+    return vmx_ks
+
+
+def check_files(vmx, dict):
+    try:
+        with open(vmx, 'r') as data:
+            lines = data.readlines()
+    except (OSError, IOError):
+        sys.exit('[-] Cannot read from file ' + vmx)
+
+    if 'encryption.keySafe' not in lines[2]:
+        sys.exit('[-] Invalid VMX file or the VMX is not encrypted')
+
+    try:
+        passf = open(dict, 'rb')
+    except IOError:
+        print('[-] Cannot open wordlist (%s)' % dict)
+        exit(1)
+
+
+def pyvmx(vmx, dict):
+
+    print("Starting pyvmx-cracker...\n")
+
+    # Some validation...
+    check_files(vmx, dict)
+    # Map KeyStore to Dict
+    parsed_ks = parse_keysafe(vmx)
+    # Print info
+    print(parsed_ks)
+    print_ksdata(parsed_ks)
+    # Crack keysafe
+    crack_keysafe(parsed_ks, dict)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Simple tool to crack VMware VMX encryption passwords")
+
+    # Add arguments
+    parser.add_argument("-v", "--vmx", dest="vmx", action="store",
+                        help=".vmx file", type=str)
+
+    parser.add_argument("-d", "--dict", dest="dict", action="store",
+                        help="password list", type=str)
+
+    args = parser.parse_args()
+
+    if args.vmx and args.dict:
+        pyvmx(args.vmx, args.dict)
+    else:
+        parser.print_help()
+        exit(1)
